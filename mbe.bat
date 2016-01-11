8362674df0ecbb415c812c5db7d65886bb40b14a
javajigi <javajigi@gmail.com>
2016-01-11 14:25:50 +0900

    불필요한 파일에 대한 ignore 처리 및 path 설정을 위한 shell 추가함.

---
+++ mbe.bat
@@ -0,0 +1,4 @@
+@echo off
+set PATH=%PATH%;C:\next-workspace\play-1.2.4
+echo "Starting MBE Project!"
+cmd 
---
+++ mbe.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+export PLAY_HOME=~/my-workspace/web-workspace/play-1.2.4
+export PATH=$PATH:$PLAY_HOME
+echo "Starting MBE Project"
